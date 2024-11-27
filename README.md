2fde57ce6497fbfd6fcf404733141a4c83a00d90
rahem <reza.rahem224@gmail.com>
2024-11-27 19:29:16 +0330

    update deploy

--- README.md
+++ README.md
@@ -3,3 +3,27 @@
   ```bash
   curl -o ~/deploy.sh https://raw.githubusercontent.com/rezarahem/deploy-nextjs/refs/heads/master/deploy.sh
   ```
+
+# Update package list and upgrade existing packages
+
+sudo apt update && sudo apt upgrade -y
+
+# Install Docker
+
+sudo apt install apt-transport-https ca-certificates curl software-properties-common -y
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" -y
+sudo apt update
+sudo apt install docker-ce -y
+
+# Install Docker Compose
+
+sudo rm -f /usr/local/bin/docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/v2.24.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+
+sudo chmod +x /usr/local/bin/docker-compose
+
+sudo ln -sf /usr/local/bin/docker-compose /usr/bin/docker-compose
+
+sudo systemctl enable docker
+sudo systemctl start docker
--- deploy.sh
+++ deploy.sh
@@ -1,64 +1,12 @@
 #!/bin/bash
 
-# Env Vars
-# POSTGRES_USER="myuser"
-# POSTGRES_PASSWORD=$(openssl rand -base64 12)  # Generate a random 12-character password
-# POSTGRES_DB="mydatabase"
-# SECRET_KEY="my-secret" # for the demo app
-# NEXT_PUBLIC_SAFE_KEY="safe-key" # for the demo app
-DOMAIN_NAME="ju.rahem.dev" # replace with your own
-EMAIL="reza.rahem224@example.com" # replace with your own
 
 # Script Vars
+DOMAIN_NAME="ju.rahem.dev"
+EMAIL="reza.rahem224@example.com" 
 REPO_URL="https://github.com/rezarahem/deploy-nextjs.git"
 APP_DIR=~/myapp
-# SWAP_SIZE="1G"  # Swap size of 1GB
 
-# Update package list and upgrade existing packages
-sudo apt update && sudo apt upgrade -y
-
-# Add Swap Space
-# echo "Adding swap space..."
-# sudo fallocate -l $SWAP_SIZE /swapfile
-# sudo chmod 600 /swapfile
-# sudo mkswap /swapfile
-# sudo swapon /swapfile
-
-# Make swap permanent
-# echo '/swapfile none swap sw 0 0' | sudo tee -a /etc/fstab
-
-# Install Docker
-sudo apt install apt-transport-https ca-certificates curl software-properties-common -y
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" -y
-sudo apt update
-sudo apt install docker-ce -y
-
-# Install Docker Compose
-sudo rm -f /usr/local/bin/docker-compose
-sudo curl -L "https://github.com/docker/compose/releases/download/v2.24.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-
-# Wait for the file to be fully downloaded before proceeding
-if [ ! -f /usr/local/bin/docker-compose ]; then
-  echo "Docker Compose download failed. Exiting."
-  exit 1
-fi
-
-sudo chmod +x /usr/local/bin/docker-compose
-
-# Ensure Docker Compose is executable and in path
-sudo ln -sf /usr/local/bin/docker-compose /usr/bin/docker-compose
-
-# Verify Docker Compose installation
-docker-compose --version
-if [ $? -ne 0 ]; then
-  echo "Docker Compose installation failed. Exiting."
-  exit 1
-fi
-
-# Ensure Docker starts on boot and start Docker service
-sudo systemctl enable docker
-sudo systemctl start docker
 
 # Clone the Git repository
 if [ -d "$APP_DIR" ]; then
@@ -69,23 +17,6 @@ else
   git clone $REPO_URL $APP_DIR
   cd $APP_DIR
 fi
-
-# For Docker internal communication ("db" is the name of Postgres container)
-# DATABASE_URL="postgres://$POSTGRES_USER:$POSTGRES_PASSWORD@db:5432/$POSTGRES_DB"
-
-# For external tools (like Drizzle Studio)
-# DATABASE_URL_EXTERNAL="postgres://$POSTGRES_USER:$POSTGRES_PASSWORD@localhost:5432/$POSTGRES_DB"
-
-# Create the .env file inside the app directory (~/myapp/.env)
-# echo "POSTGRES_USER=$POSTGRES_USER" > "$APP_DIR/.env"
-# echo "POSTGRES_PASSWORD=$POSTGRES_PASSWORD" >> "$APP_DIR/.env"
-# echo "POSTGRES_DB=$POSTGRES_DB" >> "$APP_DIR/.env"
-# echo "DATABASE_URL=$DATABASE_URL" >> "$APP_DIR/.env"
-# echo "DATABASE_URL_EXTERNAL=$DATABASE_URL_EXTERNAL" >> "$APP_DIR/.env"
-
-# These are just for the demo of env vars
-# echo "SECRET_KEY=$SECRET_KEY" >> "$APP_DIR/.env"
-# echo "NEXT_PUBLIC_SAFE_KEY=$NEXT_PUBLIC_SAFE_KEY" >> "$APP_DIR/.env"
 
 # Install Nginx
 sudo apt install nginx -y
@@ -165,15 +96,6 @@ if ! sudo docker-compose ps | grep "Up"; then
   exit 1
 fi
 
-# Output final message
-# The .env file has been created with the following values:
-# - POSTGRES_USER
-# - POSTGRES_PASSWORD (randomly generated)
-# - POSTGRES_DB
-# - DATABASE_URL
-# - DATABASE_URL_EXTERNAL
-# - SECRET_KEY
-# - NEXT_PUBLIC_SAFE_KEY"
 
 echo "Deployment complete. Your Next.js app and PostgreSQL database are now running. 
 Next.js is available at https://$DOMAIN_NAME, and the PostgreSQL database is accessible from the web service.
